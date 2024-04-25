b8f38e553932b03002426b30dc0b6207998a8017
Arnav Rao <90997954+ArnavElectron@users.noreply.github.com>
2024-04-25 19:34:45 +0530

    Update decrypt.py

--- Workshop 24-04-2024/HEADed XORbitantly/solution/decrypt.py
+++ Workshop 24-04-2024/HEADed XORbitantly/solution/decrypt.py
@@ -3,4 +3,4 @@ f= open('El3CTR0N.png','rb').read() #any random png image just to get the headde
 k=open('enc.png','rb').read() # given encrypted PNG image
 key=[f[i] for i in range(7)] # the header of a PNG image
 enc= bytearray([i^j for i,j in zip(k,cycle(key))])
-open("dec.png","wb").write(enc)
+open("solution.png","wb").write(enc)
