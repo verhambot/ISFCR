6ea3ba24bc8cfbb0554fdebb00f43df956fec5a9
Arnav Rao <90997954+ArnavElectron@users.noreply.github.com>
2024-04-25 18:19:51 +0530

    challenge files for HEADed XORbitatnly

---
+++ Workshop 24-04-2024/HEADed XORbitantly/encrypt.py
@@ -0,0 +1,9 @@
+def frfrrfrafrafrafrafrafarf(asdadasdasdasd):
+    while True:
+        for item in asdadasdasdasd:
+            yield item
+jhljkhjklhjkhajsdfhajshd= open('xor_chall_header\deployfiles\\rev.png','rb').read()
+asdadasdasdasd=[jhljkhjklhjkhajsdfhajshd[i] for i in range(7)]
+jkljkjlkjlkjlkjlkjljl= bytearray([i^j for i,j in zip(jhljkhjklhjkhajsdfhajshd,frfrrfrafrafrafrafrafarf(asdadasdasdasd))])
+open("enc.png","wb").write(jkljkjlkjlkjlkjlkjljl)
+
